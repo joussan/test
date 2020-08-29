895bacd2b27c897edbe0a30abb456ceb2ed512b9
joussan <joussan@gmail.com>
2020-08-29 19:25:42 +0100

    yes I can do it

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hello hassan , you can do it " . PHP_VERSION . "\n";
+print "salam ssi  hassan , keep doing it  " . PHP_VERSION . "\n";
 ?>
